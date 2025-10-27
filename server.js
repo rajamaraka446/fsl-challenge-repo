55a022638778e035792c63ce618474e04d7affef
Raja Reddy <rajamaraka446@gmail.com>
2025-10-28 00:24:00 +0530

    final updated version of my application

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
-app.get('/', (req, res) => res.send(`Hello this is fsl-challenge completed by me "RAJA" | Secret: ${process.env.MY_SECRET || 'not-set'}`));
+app.get('/', (req, res) => res.send(`  Hello this is fsl-challenge completed by me "RAJA" | Secret: ${process.env.MY_SECRET || 'not-set'}`));
 app.listen(port, () => console.log(`Running on ${port}`));
 
